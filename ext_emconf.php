23536e11d4f3a9f6a4e8881e282601038e671eca
Sven Wappler <wappler@wappler.systems>
2025-02-04 14:58:21 +0100

    init

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,8 +1,8 @@
 <?php
 
-$EM_CONF['t3bootstrap-news'] = [
-    'title' => 'Template Patch für news',
-    'description' => 'Adds the t3bootstrap TCA palettes to the news plugins',
+$EM_CONF['t3bootstrap-blog'] = [
+    'title' => 'Template patch für blog',
+    'description' => 't3bootstrap modifications for the blog extension',
     'author' => 'Sven Wappler',
     'author_email' => 'typo3YYYY@wappler.systems',
     'category' => 'fe',
@@ -14,8 +14,7 @@ $EM_CONF['t3bootstrap-news'] = [
         'depends' => [
             'typo3' => '13.0.0-13.4.99',
             'ws_t3bootstrap' => '13.0.0-13.99.99',
-            'news' => '12.0.0',
-            'numbered_pagination' => '2.0.0'
+            'blog' => '13.0.0',
         ],
     ],
 ];
